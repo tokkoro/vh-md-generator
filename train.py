bdc2f59df06c168247ad21a2a02c87b38ece2bf2
Tomi Kokkonen <tomi@valohai.com>
2021-04-07 15:11:22 +0300

    functionalize

--- train.py
+++ train.py
@@ -10,6 +10,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument('--how_random', type=float, default=0.05)
 how_random = parser.parse_args().how_random
 
+
 def logMetadata(epoch, loss, accuracy):
     print()
     print(json.dumps({
@@ -19,8 +20,20 @@ def logMetadata(epoch, loss, accuracy):
     }))
 
 
-for epoch in range(1, end_epoch + 1):
-    logMetadata(epoch, 4 - 2 * epoch / end_epoch + random.random() * how_random,
-                max(0.0, (1 + math.log10(max(0.000000001, (epoch - 1) / end_epoch)) / 10) + (
-                            how_random * 2.0 * (random.random() - 0.5))))
+def lerp(a, b, t):
+    return a * (1 - t) + b * t
+
+
+def steep_log01(t):
+    return max(0.0, (1 + math.log10(max(0.000000001, t)) / 10))
+
+
+def random_m1_p1():
+    return 2.0 * (random.random() - 0.5)
+
+
+for epoch in range(0, end_epoch):
+    t = epoch / end_epoch
+    print(t)
+    logMetadata(epoch + 1, lerp(4.0, 2.0, t) + random.random() * how_random, steep_log01(t) + (how_random * random_m1_p1()))
     time.sleep(0.8 + 0.4 * random.random())
