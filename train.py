da3b56b0253400032a75f331cc1a0d4d3396729b
Tomi Kokkonen <tomi@valohai.com>
2021-04-07 11:55:38 +0300

    add how_random parameter

--- train.py
+++ train.py
@@ -1,10 +1,14 @@
 import json
-import math
 import random
 import time
+import math
+import argparse
 
 end_epoch = 10
 
+parser = argparse.ArgumentParser()
+parser.add_argument('--how_random', type=float, default=0.05)
+how_random = parser.parse_args().how_random
 
 def logMetadata(epoch, loss, accuracy):
     print()
@@ -15,7 +19,8 @@ def logMetadata(epoch, loss, accuracy):
     }))
 
 
-for epoch in range(0, end_epoch + 1):
-    logMetadata(epoch, 4 - 0.1 * epoch / end_epoch + random.random() * 0.3,
-                math.sqrt(max(0.0, epoch / end_epoch - 0.1 * random.random())))
+for epoch in range(1, end_epoch + 1):
+    logMetadata(epoch, 4 - 2 * epoch / end_epoch + random.random() * how_random,
+                max(0.0, (1 + math.log10(max(0.000000001, (epoch - 1) / end_epoch)) / 10) + (
+                            how_random * 2.0 * (random.random() - 0.5))))
     time.sleep(0.8 + 0.4 * random.random())
