45e30cd0067474bf83cfdbf879fc9793f5273b16
Tomi Kokkonen <tomi@valohai.com>
2022-02-12 09:13:39 +0200

    Add more ids

--- output_pipeline_task_storeid.py
+++ output_pipeline_task_storeid.py
@@ -5,7 +5,7 @@ f = open("/valohai/outputs/output_file.txt", "w")
 f.write("this is an output!")
 f.close()
 
-storeids = [122, 799, 154, 209, 916, 444, 345]  # 209 will error in error task
+storeids = [122, 799, 154, 209, 916, 444, 345] * 30  # 209 will error in error task
 
 # Print out a list as metadata
 print(json.dumps({
