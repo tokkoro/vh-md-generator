0efccba44615eae103fe8641b19c794bf7f36186
Tomi Kokkonen <tomi@valohai.com>
2022-06-16 10:36:22 +0300

    add array metadata

--- train.py
+++ train.py
@@ -1,18 +1,19 @@
 import json
 
 
-def logMetadata(epoch, loss, accuracy, gain, num):
+def logMetadata(epoch, loss, accuracy, gain, num, arr):
     print(json.dumps({
         'epoch': epoch,
         'loss': loss,
         'acc': accuracy,
         'gain': gain,
         'numberi': num,
+        'arrrrray': arr
     }))
 
 
-logMetadata(1, "teksti",    "3",    '1970-01-01T00:00:00.000000', 1)
-logMetadata(2, -1,          4,      '2020-01-01T00:00:00.000000', "2")
-logMetadata(3, "teksti",    5,      '2020-01-01T00:00:00.000000', 3)
-logMetadata(4, 3,           6,      '2020-01-01T00:00:00.000000', "5")
-logMetadata(4, "teksti",    6,      '2020-01-01T00:00:00.000000', "5")
+logMetadata(1, "teksti",    "3",    '1970-01-01T00:00:00.000000', 1,        [[10, 2, 3], [4, 5, 6], [7, 8, 9]])
+logMetadata(2, -1,          4,      '2020-01-01T00:00:00.000000', "2",      [[11, 2, 3], [4, 5, 6], [7, 8, 9]])
+logMetadata(3, "teksti",    5,      '2020-01-01T00:00:00.000000', 3,        [[12, 2, 3], [4, 5, 6], [7, 8, 9]])
+logMetadata(4, 3,           6,      '2020-01-01T00:00:00.000000', "5",      [[13, 2, 3], [4, 5, 6], [7, 8, 9]])
+logMetadata(5, "teksti",    6,      '2020-01-01T00:00:00.000000', "5",      [[14, 2, 3], [4, 5, 6], [7, 8, 9]])
