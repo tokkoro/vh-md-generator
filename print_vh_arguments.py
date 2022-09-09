8cdf070e731ef963ab71ddc0dc838c296618ec14
Tomi Kokkonen <tomi@valohai.com>
2022-09-09 11:26:41 +0300

    add string list output prints

--- print_vh_arguments.py
+++ print_vh_arguments.py
@@ -1,4 +1,16 @@
+import valohai
 from valohai.internals import global_state, global_state_loader
 
 global_state_loader.load_global_state_if_necessary()
 print(global_state.parameters)
+
+mylist = valohai.parameters('mylist-separated').value
+print(type(mylist))
+print(mylist)
+print(mylist[0])
+
+mylist_non = valohai.parameters('mylist-non-separated').value
+print(type(mylist_non))
+print(mylist_non)
+print(mylist_non[0])
+
