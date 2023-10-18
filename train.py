0e905c03d3cac44313759cf43d034b9fe9d90b44
Tomi Kokkonen <tomi@valohai.com>
2023-10-18 15:41:15 +0300

    create empty file

--- train.py
+++ train.py
@@ -1,8 +1,6 @@
-import json
-import random
-import time
-import math
+import valohai
 import argparse
+from pathlib import Path
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--epochs', type=int, default=10)
@@ -13,29 +11,6 @@ end_epoch = parsed.epochs
 how_random = parsed.how_random
 wait_time = parsed.wait_time
 
+out_path = valohai.outputs().path('filu.txt')
 
-def logMetadata(epoch, loss, accuracy):
-    print(json.dumps({
-        'epoch': epoch,
-        'loss': loss,
-        'acc': accuracy,
-    }))
-
-
-def lerp(a, b, t):
-    return a * (1 - t) + b * t
-
-
-def steep_log01(t):
-    return max(0.0, (1 + math.log10(max(0.000000001, t)) / 10))
-
-
-def random_m1_p1():
-    return 2.0 * (random.random() - 0.5)
-
-
-for epoch in range(0, end_epoch):
-    t = epoch / end_epoch
-    logMetadata(epoch + 1, lerp(4.0, 2.0, t) + random.random() * how_random,
-                steep_log01(t) + (how_random * random_m1_p1()))
-    time.sleep(wait_time * (0.8 + 0.4 * random.random()))  # +- 20 % random
+Path(out_path).touch()
