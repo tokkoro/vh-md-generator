556791ba050f046473388419822042dcea75d63c
tokko <tomi@valohai.com>
2025-09-05 08:33:44 +0300

    Add filename prefix

--- train.py
+++ train.py
@@ -13,8 +13,10 @@ def random_date(start, end):
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--files', type=int, default=10)
+parser.add_argument('--prefix', type=str, default="price_TOMI")
 parsed = parser.parse_args()
 file_count = parsed.files
+prefix = parsed.prefix
 version_name = f"{random_date(datetime(2020, 1, 1), datetime.now())}".replace(" ", "-").replace(":", "-")
 
 metadata = {
@@ -26,10 +28,10 @@ metadata = {
 }
 
 for i in range(file_count):
-    f = open("/valohai/outputs/price_TOMI_" + str(i) + ".csv", "w")
+    f = open(f"/valohai/outputs/{prefix}" + str(i) + ".csv", "w")
     f.write(f"timestamp,price_TOMI_{i+1}\n{random_date(datetime(2020, 1, 1), datetime.now())},{random.random() * 100}\n")
     f.close()
-    fmd = open("/valohai/outputs/price_TOMI_" + str(i) + ".csv.metadata.json", "w")
+    fmd = open(f"/valohai/outputs/{prefix}" + str(i) + ".csv.metadata.json", "w")
     fmd.write(json.dumps(metadata))
     fmd.close()
 
